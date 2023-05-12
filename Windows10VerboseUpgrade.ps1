eac3c9c750a99997b9777a8d1928a77d7c002b65
Jetboy01 <30339308+Jetboy01@users.noreply.github.com>
2023-05-12 15:08:51 +0100

    Update Windows10VerboseUpgrade.ps1

--- Windows10VerboseUpgrade.ps1
+++ Windows10VerboseUpgrade.ps1
@@ -6,5 +6,3 @@ $url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
 $file = "c:\windows\temp\Win10Upgrade.exe" 
 $webClient.DownloadFile($url,$file) 
 Start-Process -FilePath $file -ArgumentList '/skipeula /auto upgrade /copylogs $dir'
-
-Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /copylogs $dir'
