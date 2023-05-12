5a7a935a00b60da75ff6bb3148c0d8098a737899
Jetboy01 <30339308+Jetboy01@users.noreply.github.com>
2023-05-12 15:08:38 +0100

    Update Windows10SilentUpgrade.ps1

--- Windows10SilentUpgrade.ps1
+++ Windows10SilentUpgrade.ps1
@@ -5,6 +5,4 @@ $webClient = New-Object System.Net.WebClient
 $url = 'https://go.microsoft.com/fwlink/?LinkID=799445' 
 $file = "c:\windows\temp\Win10Upgrade.exe" 
 $webClient.DownloadFile($url,$file) 
-Start-Process -FilePath $file -ArgumentList '/skipeula /auto upgrade /copylogs $dir'
-
 Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /copylogs $dir'
