de52e195859df5427eab433bb009d8b4748816f6
Jetboy01 <30339308+Jetboy01@users.noreply.github.com>
2023-05-12 14:46:23 +0100

    Update and rename Windows10AttendedUpgrade.ps1 to Windows10Upgrade.ps1

---
+++ Windows10Upgrade.ps1
@@ -0,0 +1,25 @@
+<#
+    Run a Windows 10 Update directly from PowerShell
+#>
+param
+(
+    # Silent, or Verbose
+   [Parameter]
+   [ValidateSet("Yes","No")]
+   $ShowGUI
+)
+
+$webClient = New-Object System.Net.WebClient 
+$url = 'https://go.microsoft.com/fwlink/?LinkID=799445' 
+$file = "c:\windows\temp\Win10Upgrade.exe" 
+$webClient.DownloadFile($url,$file) 
+Start-Process -FilePath $file -ArgumentList '/skipeula /auto upgrade /copylogs $dir'
+
+if ( $ShowGUI -eq "no" )
+{
+    Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /copylogs $dir'  
+}
+else
+{
+    Start-Process -FilePath $file -ArgumentList '/skipeula /auto upgrade /copylogs $dir'
+}
