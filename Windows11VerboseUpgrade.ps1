924b23d341b79fcfa596a75f3fa78ba6719b072f
Jetboy01 <30339308+Jetboy01@users.noreply.github.com>
2023-05-15 14:22:57 +0100

    Update Windows11VerboseUpgrade.ps1

--- Windows11VerboseUpgrade.ps1
+++ Windows11VerboseUpgrade.ps1
@@ -3,6 +3,6 @@
 #>
 $webClient = New-Object System.Net.WebClient 
 $url = 'https://go.microsoft.com/fwlink/?linkid=2171764' 
-$file = "c:\windows\temp\Win10Upgrade.exe" 
+$file = "c:\windows\temp\Win11Upgrade.exe" 
 $webClient.DownloadFile($url,$file) 
 Start-Process -FilePath $file -ArgumentList '/skipeula /auto upgrade /copylogs $dir'
