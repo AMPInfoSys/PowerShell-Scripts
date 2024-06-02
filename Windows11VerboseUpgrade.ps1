b3e68e1f95d80c3965f9c9a692b76bdd6bbdeab9
Jetboy01 <30339308+Jetboy01@users.noreply.github.com>
2024-06-02 20:49:39 +0100

    Update Windows11VerboseUpgrade.ps1

--- Windows11VerboseUpgrade.ps1
+++ Windows11VerboseUpgrade.ps1
@@ -1,8 +1,11 @@
 <#
     Run a Windows 11 Update directly from PowerShell
+
+    iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/AMPInfoSys/PowerShell-Scripts/main/Windows11VerboseUpgrade.ps1'))
 #>
 $webClient = New-Object System.Net.WebClient 
 $url = 'https://go.microsoft.com/fwlink/?linkid=2171764' 
 $file = "c:\windows\temp\Win11Upgrade.exe" 
 $webClient.DownloadFile($url,$file) 
 Start-Process -FilePath $file -ArgumentList '/skipeula /auto upgrade /copylogs $dir'
+
