258313200e0ed815d78367c3bf5c44149582ec14
Jetboy01 <30339308+Jetboy01@users.noreply.github.com>
2025-05-19 09:50:09 +0100

    Update Get_CPU_Passmark.ps1

--- Get_CPU_Passmark.ps1
+++ Get_CPU_Passmark.ps1
@@ -1,10 +1,23 @@
+if (-not (Get-Module -ErrorAction Ignore -ListAvailable PowerHTML))
+{
+    Write-Host "Installing PowerHTML module"
+    Install-Module PowerHTML -Scope CurrentUser -ErrorAction Stop
+}
+
+Import-Module -ErrorAction Stop PowerHTML
+
+
 #Get the CPU name and scrape the benchmark score from cpubenchmark.net
 
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
 $CPU = Get-WMIObject win32_Processor | select name
 $url ='https://www.cpubenchmark.net/cpu.php?cpu='+$CPU.Name
-$response = Invoke-WebRequest -Uri $url
-$classname ='right-desc'
-$PassmarkText = $response.ParsedHtml.body.getElementsByClassName($classname) | select -expand innertext
-$PassmarkText = $PassmarkText -split "\n"
-write-host $PassmarkText[2]
+
+$webClient = New-Object Net.WebClient
+$webClient.Headers.Add('user-agent', [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox)
+$result = $webClient.DownloadString($url)
+$html = ConvertFrom-Html -Content $result
+
+$PassmarkText = $html.selectNodes("/html/body/div[3]/div[1]/div/div[3]/div[2]/div[2]/div[1]/div/div[2]/div[3]")
+
+write-host $PassmarkText.InnerText.Trim()
